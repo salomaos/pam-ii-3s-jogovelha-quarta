1a474ca0ccd81c44c3d94aadca7853eeff30538c
salomaos <salomao.ssn@gmail.com>
2025-05-22 10:16:43 -0300

    estrutura básica

---
+++ app/index.tsx
@@ -0,0 +1,48 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const nome = "Salomão";
+
+  return (
+    <View>
+      <Text>Jogo da Velha</Text>
+      <Text>Vencedor</Text>
+
+      <View>
+        <View>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
